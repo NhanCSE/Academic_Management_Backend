98dc3b65ff7f0b38e998a3d287b728178fb822bf
NhanCSE <tranthenhan.cse@gmail.com>
2024-05-07 07:17:16 +0700

    set up backup server

--- firebase/firebaseConnection.js
+++ firebase/firebaseConnection.js
@@ -4,10 +4,9 @@ var serviceAccount = require("../ServiceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  // databaseURL: "https://nodejs-a11db-default-rtdb.firebaseio.com",
   //databaseURL: "https://testbe-28a98-default-rtdb.firebaseio.com/",
+  // storageBucket: "gs://testbe-28a98.appspot.com"
   databaseURL: "https://academicmanagenent-default-rtdb.firebaseio.com",
-  // storageBucket: "gs://academic-management-backend.appspot.com"
   storageBucket: "gs://academicmanagenent.appspot.com"
 });
 
